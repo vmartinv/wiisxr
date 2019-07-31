fc8d9fc3421035d302e113d10f689179c003d0c9
Martin Villagra <mvillagra0@gmail.com>
2019-07-31 13:18:46 -0300

    Fix CRLF

--- Gamecube/PlugPAD.c
+++ Gamecube/PlugPAD.c
@@ -163,6 +163,15 @@ void auto_assign_controllers(void)
 	}
 }
 
+static inline button_t* getPointer(FILE *f, button_t* list, int size){
+	int index;
+	fread(&index, 4, 1, f);
+	return list + (index % size);
+}
+static inline button_t* getButton(FILE *f, controller_t* controller){
+	return getPointer(f, controller->buttons, controller->num_buttons);
+}
+
 int load_configurations(FILE* f, controller_t* controller){
 	int i;
 	char magic[4] = { 
@@ -173,42 +182,33 @@ int load_configurations(FILE* f, controller_t* controller){
 	if(memcmp(magic, actual, 4))
 		return 0;
 	
-	inline button_t* getPointer(button_t* list, int size){
-		int index;
-		fread(&index, 4, 1, f);
-		return list + (index % size);
-	}
-	inline button_t* getButton(void){
-		return getPointer(controller->buttons, controller->num_buttons);
-	}
-	
 	for(i=0; i<4; ++i){
-		controller->config_slot[i].SQU = getButton();
-		controller->config_slot[i].CRO = getButton();
-		controller->config_slot[i].CIR = getButton();
-		controller->config_slot[i].TRI = getButton();
+		controller->config_slot[i].SQU = getButton(f, controller);
+		controller->config_slot[i].CRO = getButton(f, controller);
+		controller->config_slot[i].CIR = getButton(f, controller);
+		controller->config_slot[i].TRI = getButton(f, controller);
 		
-		controller->config_slot[i].R1 = getButton();
-		controller->config_slot[i].L1 = getButton();
-		controller->config_slot[i].R2 = getButton();
-		controller->config_slot[i].L2 = getButton();
-		controller->config_slot[i].R3 = getButton();
-		controller->config_slot[i].L3 = getButton();
+		controller->config_slot[i].R1 = getButton(f, controller);
+		controller->config_slot[i].L1 = getButton(f, controller);
+		controller->config_slot[i].R2 = getButton(f, controller);
+		controller->config_slot[i].L2 = getButton(f, controller);
+		controller->config_slot[i].R3 = getButton(f, controller);
+		controller->config_slot[i].L3 = getButton(f, controller);
 
-		controller->config_slot[i].DL = getButton();
-		controller->config_slot[i].DR = getButton();
-		controller->config_slot[i].DU = getButton();
-		controller->config_slot[i].DD = getButton();
+		controller->config_slot[i].DL = getButton(f, controller);
+		controller->config_slot[i].DR = getButton(f, controller);
+		controller->config_slot[i].DU = getButton(f, controller);
+		controller->config_slot[i].DD = getButton(f, controller);
 
-		controller->config_slot[i].START  = getButton();
-		controller->config_slot[i].SELECT = getButton();
+		controller->config_slot[i].START  = getButton(f, controller);
+		controller->config_slot[i].SELECT = getButton(f, controller);
 				
 		controller->config_slot[i].analogL = 
-			getPointer(controller->analog_sources, controller->num_analog_sources);
+			getPointer(f, controller->analog_sources, controller->num_analog_sources);
 		controller->config_slot[i].analogR = 
-			getPointer(controller->analog_sources, controller->num_analog_sources);
+			getPointer(f, controller->analog_sources, controller->num_analog_sources);
 		controller->config_slot[i].exit =
-			getPointer(controller->menu_combos, controller->num_menu_combos);
+			getPointer(f, controller->menu_combos, controller->num_menu_combos);
 		fread(&controller->config_slot[i].invertedYL, 4, 1, f);
 		fread(&controller->config_slot[i].invertedYR, 4, 1, f);
 	}
